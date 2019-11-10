69a2cb2546ccfa7a55a296e8954200c71cf06b5b
Brandon U <unzueta.brandon@gmai.com>
2019-11-09 17:02:02 -0800

    adding some basic routes and a user model

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
+//Connect to MongoDataBase
+connectDB();
+
 app.get("/", (req, res) => res.send("API is running"));
+
+//Defined Routes
+app.use("/api/users", require("./routes/api/users"));
 
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}.`));
