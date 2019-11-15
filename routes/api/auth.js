8ea909698d904ac7fff8dbfa8cb556a630aabd6e
Brandon U <unzueta.brandon@gmai.com>
2019-11-14 19:32:59 -0800

    setting uo auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
 // @route     GET api/users
 // @desc      Test route
 // @access    Public
-router.get("/", (req, res) => res.send("User route"));
+router.get("/", auth, (req, res) => res.send("Auth route"));
 
 module.exports = router;
