52253a9edb6b8eb25165427fb203fd6e4b2a79d6
Brandon U <unzueta.brandon@gmai.com>
2020-03-05 16:57:08 -0800

    working on the profile page reducers pt1

---
+++ client/src/actions/profile.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { setAlert } from "./alert";
+import { GET_PROFILE, PROFILE_ERROR } from "./types";
+
+//////////////////////////////
+//GET CURRENT USER'S PROFILE//
+//////////////////////////////
+export const getCurrentProfile = () => async;
